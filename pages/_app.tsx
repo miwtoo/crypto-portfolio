b043a1a1d3e2d777b1cd6658a5ea0b6cd889ba5b
Miwtoo <miwyugioh@gmail.com>
2021-10-17 17:03:25 +0700

    feat: :sparkles: init mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/theme";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material";
+import React from "react";
+import Head from "next/head";
+import theme from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <React.StrictMode>
+      <Head>
+        <title>Crypto Portfolio</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.StrictMode>
+  );
 }
-export default MyApp
+export default MyApp;
---
+++ styles/theme.ts
@@ -0,0 +1,24 @@
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0052cc",
+    },
+    secondary: {
+      main: "#edf2ff",
+    },
+  },
+});
+
+theme = createTheme(theme, {
+  palette: {
+    info: {
+      main: theme.palette.secondary.main,
+    },
+  },
+});
+
+theme = responsiveFontSizes(theme);
+
+export default theme;
